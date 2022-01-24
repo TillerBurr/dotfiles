a9282123ca2c264d5332445615330e99a0b91d50
Tyler Baur <49296311+baurt@users.noreply.github.com>
2022-01-24 14:13:56 -0600

    Update poetry.exclude.sh

--- poetry.exclude.sh
+++ poetry.exclude.sh
@@ -7,7 +7,7 @@ curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-
 
 # echo "Do not forget to set the PATH for pyenv to work correctly afterwards!"
 
-export PATH="$HOME/.poetry/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 
 poetry config virtualenvs.create true
 poetry config virtualenvs.in-project true
