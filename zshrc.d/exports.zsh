174f9011d03756a215c2d0e2fd57f4fa35574389
Tyler Baur <tbaur@teuteberg.com>
2022-01-24 15:07:06 -0600

    changes

--- zshrc.d/exports.zsh
+++ zshrc.d/exports.zsh
@@ -11,3 +11,6 @@ export PATH="$HOME/.local/bin:$PATH"
 export LIBGL_ALWAYS_INDIRECT=1
 export GPG_TTY=$(tty)        
 export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
