b3debdf736b7544cf3920509ab9afd39308f4092
Tyler Baur <tbaur@teuteberg.com>
2022-03-10 16:26:26 -0600

    append /usr/bin to end of path

--- zshrc.d/exports.zsh
+++ zshrc.d/exports.zsh
@@ -3,13 +3,13 @@
 
 export PATH="$HOME/.pyenv/bin:$PATH"
 export PATH="$HOME/.poetry/bin:$PATH"
-export PATH=/usr/bin:$PATH
+export PATH=$PATH:/usr/bin
 export TMPDIR=/tmp
 export PATH="$(yarn global bin):$PATH"
 export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export LIBGL_ALWAYS_INDIRECT=1
-export GPG_TTY=$(tty)        
+export GPG_TTY=$(tty)
 #export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
 export MCFLY_FUZZY=2
 export NVM_DIR="$HOME/.nvm"
