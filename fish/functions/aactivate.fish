38cc91ca3f9cc58231dc9611e4906f137a5b1ded
Tyler Baur <tbaur@teuteberg.com>
2022-11-15 09:05:39 -0600

    auto activate,

--- fish/functions/aactivate.fish
+++
@@ -1,9 +0,0 @@
-function aactivate --on-event fish_postexec
-    if set -q VIRTUAL_ENV && not test -e .venv/bin/activate.fish
-        deactivate
-    end
-    if test -e .venv/bin/activate.fish && not set -q VIRTUAL_ENV
-        source .venv/bin/activate.fish
-    end
-
-end
