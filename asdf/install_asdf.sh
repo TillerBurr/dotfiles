fa71e1e6e24e13168080ee95e80630c662395aa3
Tyler Baur <49296311+baurt@users.noreply.github.com>
2022-12-26 18:25:55 +0000

    remove asdf fish completions and add them to the link directive

--- asdf/install_asdf.sh
+++ asdf/install_asdf.sh
@@ -4,4 +4,3 @@ ASDF_BRANCH=v0.10.2
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch $ASDF_BRANCH
 
-mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
