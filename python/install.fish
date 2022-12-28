e12ac83768d81c161518e4c050fd5fe0bc93b8a1
Tyler Baur <baur.tyler@gmail.com>
2022-12-27 20:07:56 -0600

    add pipx installation, remove dotbot-pip

--- python/install.fish
+++ python/install.fish
@@ -2,3 +2,7 @@
 source ~/.asdf/asdf.fish
 asdf plugin add pipx https://github.com/amrox/asdf-pyapp.git
 asdf install pipx latest
+
+while read -la line
+    pipx install $line
+end < python/pipx-requirements.txt
