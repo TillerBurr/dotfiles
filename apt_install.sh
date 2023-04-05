612c407a72c0bfc017d84d819695621663d2a082
Tyler Baur <tbaur@teuteberg.com>
2023-04-05 12:17:53 -0500

    use rtx standalone installer

--- apt_install.sh
+++ apt_install.sh
@@ -73,7 +73,7 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of
 && apt update \
 && apt install gh -y
 
-wget -qO - https://rtx.pub/gpg-key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/rtx-archive-keyring.gpg 1> /dev/null
-echo "deb [signed-by=/usr/share/keyrings/rtx-archive-keyring.gpg arch=amd64] https://rtx.pub/deb stable main" | sudo tee /etc/apt/sources.list.d/rtx.list
-sudo apt update
-sudo apt install -y rtx
+# wget -qO - https://rtx.pub/gpg-key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/rtx-archive-keyring.gpg 1> /dev/null
+# echo "deb [signed-by=/usr/share/keyrings/rtx-archive-keyring.gpg arch=amd64] https://rtx.pub/deb stable main" | sudo tee /etc/apt/sources.list.d/rtx.list
+# sudo apt update
+# sudo apt install -y rtx
--- fish/config.fish
+++ fish/config.fish
@@ -19,9 +19,9 @@ set -x UID $(id -u)
 set -x GID $(id -g)
 set -x UNAME $(whoami)
 # set -x aactivator (which aactivator)/bin/activator
-source ~/.asdf/asdf.fish
+# source ~/.asdf/asdf.fish
 
 starship init fish | source
 scheme set default
 set -x RTX_CONFIG_FILE $HOME/.config/.rtx.toml
-/usr/bin/rtx activate fish | source
+/home/tbaur/.local/share/rtx/bin/rtx activate -s fish | source
